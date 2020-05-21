92b46480eb442d30003ff1c1c95d0387e758fecf
Anas <anasmez@outlook.es>
2020-05-21 20:16:30 +0200

    Vídeo 20: Implementando 'LIKES'

---
+++ app/Http/Controllers/StatusLikeController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StatusLikeController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 Route::view('/', 'welcome')->name('home');
 
+// Statuses routes
 Route::get('statuses', 'StatusesController@index')->name('statuses.index');
 Route::post('statuses', 'StatusesController@store')->name('statuses.store');
+
+// Statuses Likes routes
+Route::post('statuses/{status}/likes', 'StatusLikesController@store')->name('statuses.likes.store')->middleware('auth');
 
 Route::auth();
