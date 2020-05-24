6af3ad228f016cf36e80fb27ff76845d0a498178
Anas <anasmez@outlook.es>
2020-05-24 06:14:50 +0200

    Vídeo 29: Reestructuración de componentes

---
+++ resources/assets/js/components/LikeBtn.vue
@@ -0,0 +1,50 @@
+<template>
+    <button class="btn btn-link btn-sm"
+            v-if="status.is_liked"
+            dusk="unlike-btn"
+            @click="unlike(status)">
+        <strong>
+            <i class="fa fa-thumbs-up text-primary mr-1"></i>
+            TE GUSTA
+        </strong>
+    </button>
+
+    <button class="btn btn-link btn-sm"
+            v-else
+            dusk="like-btn"
+            @click="like(status)">
+        <i class="far fa-thumbs-up text-primary mr-1"></i>
+        ME GUSTA
+    </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            status: {
+                type: Object,
+                required: true
+            },
+        },
+        methods: {
+            like(status) {
+                axios.post(`/statuses/${status.id}/likes`)
+                    .then(respuesta => {
+                        status.is_liked = true;
+                        status.likes_count++;
+                    })
+            },
+            unlike(status) {
+                axios.delete(`/statuses/${status.id}/likes`)
+                    .then(respuesta => {
+                        status.is_liked = false;
+                        status.likes_count--;
+                    })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
