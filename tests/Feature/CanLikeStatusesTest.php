960bbd60f674035c775e1c32da0042a2530efd47
Anas <anasmez@outlook.es>
2020-05-25 18:25:58 +0200

    Vídeo 36: Likes polimórficos - Parte 1

--- tests/Feature/CanLikeStatusesTest.php
+++ tests/Feature/CanLikeStatusesTest.php
@@ -19,9 +19,9 @@ class CanLikeStatusesTest extends TestCase
      */
     public function guests_users_can_not_like_statuses()
     {
-        $status=factory(Status::class)->create();
+        $status = factory(Status::class)->create();
 
-        $response=$this->postJson(route('statuses.likes.store', $status));
+        $response = $this->postJson(route('statuses.likes.store', $status));
 
         $response->assertStatus(401);
     }
@@ -29,35 +29,21 @@ class CanLikeStatusesTest extends TestCase
     /**
      * @test
      */
-    function an_authenticated_user_can_like_statuses()
+    function an_authenticated_user_can_like_and_unlike_statuses()
     {
-        $this->withoutExceptionHandling();
         $user = factory(User::class)->create();
         $status = factory(Status::class)->create();
 
+        $this->assertCount(0, $status->likes);
+
         $this->actingAs($user)->postJson(route('statuses.likes.store', $status));
 
-        $this->assertDatabaseHas('likes', [
-            'user_id' => $user->id,
-            'status_id' => $status->id
-        ]);
-    }
-    /**
-     * @test
-     */
-    function an_authenticated_user_can_unlike_statuses()
-    {
-        $this->withoutExceptionHandling();
+        $this->assertCount(1, $status->fresh()->likes);
 
-        $user=factory(User::class)->create();
-        $status=factory(Status::class)->create();
-        $this->actingAs($user)->postJson(route('statuses.likes.store', $status));
+        $this->assertDatabaseHas('likes', ['user_id' => $user->id]);
 
         $this->actingAs($user)->deleteJson(route('statuses.likes.destroy', $status));
-
-        $this->assertDatabaseMissing('likes', [
-            'user_id'=>$user->id,
-            'status_id'=>$status->id
-        ]);
+        $this->assertCount(0, $status->fresh()->likes);
+        $this->assertDatabaseMissing('likes', ['user_id' => $user->id]);
     }
 }
--- tests/Unit/Models/CommentTest.php
+++ tests/Unit/Models/CommentTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Models;
 
 use App\Models\Comment;
+use App\Models\Like;
 use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -10,13 +11,29 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class CommentTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * @test
      */
     public function a_comment_belongs_to_a_user()
     {
-        $comment=factory(Comment::class)->create();
+        $comment = factory(Comment::class)->create();
 
         $this->assertInstanceOf(User::class, $comment->user);
+    }
+
+    /**
+     * @test
+     */
+    function a_comment_has_many_likes()
+    {
+        $comment = factory(Comment::class)->create();
+
+        factory(Like::class)->create([
+            'likeable_id' => $comment->id,          // 1
+            'likeable_type' => get_class($comment) // App\\Models\\Comment
+        ]);
+
+        $this->assertInstanceOf(Like::class, $comment->likes->first());
     }
 }
