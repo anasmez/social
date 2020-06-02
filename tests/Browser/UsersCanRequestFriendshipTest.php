15bbc093c0a118ef0c3111a68e0dd3ffe68df948
Anas <anasmez@outlook.es>
2020-06-02 08:38:00 +0200

    Vídeo 54: Aceptar solicitud con Vue.js

--- tests/Browser/UsersCanRequestFriendshipTest.php
+++ tests/Browser/UsersCanRequestFriendshipTest.php
@@ -2,10 +2,11 @@
 
 namespace Tests\Browser;
 
+use App\Models\Friendship;
 use App\User;
-use Tests\DuskTestCase;
-use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
 
 class UsersCanRequestFriendshipTest extends DuskTestCase
 {
@@ -27,9 +28,30 @@ class UsersCanRequestFriendshipTest extends DuskTestCase
                 ->assertSee('Cancelar solicitud')
                 ->press('@request-friendship')
                 ->waitForText('Solicitar amistad')
-                ->assertSee('Solicitar amistad')
+                ->assertSee('Solicitar amistad');
+        });
+    }
 
-            ;
+    /** @test */
+    public function recipients_can_accept_and_deny_friendship_requests()
+    {
+        $sender = factory(User::class)->create();
+        $recipient = factory(User::class)->create();
+
+        Friendship::create([
+            'sender_id' => $sender->id,
+            'recipient_id' => $recipient->id,
+        ]);
+
+        $this->browse(function (Browser $browser) use ($sender, $recipient) {
+            $browser->loginAs($recipient)
+                ->visit(route('accept-friendships.index'))
+                ->assertSee($sender->name)
+                ->press('@accept-friendship')
+                ->waitForText('son amigos')
+                ->assertSee('son amigos')
+                ->visit(route('accept-friendships.index'))
+                ->assertSee('son amigos');
         });
     }
 }
