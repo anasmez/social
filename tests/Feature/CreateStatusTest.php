4edc13d9b3600bb84d5cdc52743bf7b2a7159523
Anas <anasmez@outlook.es>
2020-06-05 05:54:18 +0200

    Vídeo 61: Cómo testear eventos

--- tests/Feature/CreateStatusTest.php
+++ tests/Feature/CreateStatusTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Events\StatusCreated;
+use App\Http\Resources\StatusResource;
+use App\Models\Status;
 use App\User;
-use Symfony\Component\Mime\Header\IdentificationHeader;
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
 
 class CreateStatusTest extends TestCase
 {
@@ -17,31 +19,34 @@ class CreateStatusTest extends TestCase
      */
     public function guests_users_can_not_create_statuses()
     {
-        $response = $this->postJson(route('statuses.store'), ['body'=>'Mi primer estado']);
+        $response = $this->postJson(route('statuses.store'), ['body' => 'Mi primer estado']);
 
         $response->assertStatus(401);
+
     }
-    /**
-     * @test
-     */
+
+    /** @test */
     public function an_authenticated_user_can_create_statuses()
     {
-        $this->withoutExceptionHandling();
-        // 1. Given => Teniendo un usuario autenticado
-        $user=factory(User::class)->create();
+        Event::fake([StatusCreated::class]);
+
+        $user = factory(User::class)->create();
         $this->actingAs($user);
 
-        // 2. When => Cuando hace un post request a status
-        $response = $this->post(route('statuses.store'), ['body'=>'Mi primer estado']);
+        $response = $this->post(route('statuses.store'), ['body' => 'Mi primer estado']);
+
+        Event::assertDispatched(StatusCreated::class, function ($event){
+            return $event->status->id===Status::first()->id
+                && get_class($event->status)===StatusResource::class;
+        });
 
         $response->assertJson([
-            'data'=>['body'=>'Mi primer estado'],
+            'data' => ['body' => 'Mi primer estado'],
         ]);
 
-        // 3. Then => Entonces veo un nuevo estado en la base de datos
         $this->assertDatabaseHas('statuses', [
-            'user_id'=>$user->id,
-            'body'=>'Mi primer estado']);
+            'user_id' => $user->id,
+            'body' => 'Mi primer estado']);
     }
 
     /**
@@ -49,29 +54,30 @@ class CreateStatusTest extends TestCase
      */
     public function a_status_requires_body()
     {
-        $user=factory(User::class)->create();
+        $user = factory(User::class)->create();
         $this->actingAs($user);
 
-        $response=$this->postJson(route('statuses.store'), ['body'=>'']);
+        $response = $this->postJson(route('statuses.store'), ['body' => '']);
         $response->assertStatus(422);
 
         $response->assertJsonStructure([
-            'message', 'errors'=>['body']
+            'message', 'errors' => ['body']
         ]);
     }
+
     /**
      * @test
      */
     public function a_status_body_requires_a_minimum_length()
     {
-        $user=factory(User::class)->create();
+        $user = factory(User::class)->create();
         $this->actingAs($user);
 
-        $response=$this->postJson(route('statuses.store'), ['body'=>'asdf']);
+        $response = $this->postJson(route('statuses.store'), ['body' => 'asdf']);
         $response->assertStatus(422);
 
         $response->assertJsonStructure([
-            'message', 'errors'=>['body']
+            'message', 'errors' => ['body']
         ]);
     }
 }
